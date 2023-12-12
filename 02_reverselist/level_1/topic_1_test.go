940c1e0c28e0283c6d7ffb6f89341f5c1fde0411
Twelveeee <Twelveeee12@gmail.com>
2023-12-12 17:10:49 +0000

    feat:reverselist

--- 02_reverselist/level_1/topic_1_test.go
+++ 02_reverselist/level_1/topic_1_test.go
@@ -34,3 +34,53 @@ func TestReverseListV3(t *testing.T) {
 
 	node.PrintList()
 }
+
+func TestReverseListV4(t *testing.T) {
+	node := InitLinkNodeByList([]int{1, 2, 3, 4})
+	node.PrintList()
+
+	node = reverseListV4(node)
+	fmt.Println("**after**")
+
+	node.PrintList()
+}
+
+// 虚拟头节点
+func reverseListV4(node *LinkNode) *LinkNode {
+	dummyHeadNode := &LinkNode{}
+	dummyHeadNode.Next = node
+
+	pre, curr, temp := dummyHeadNode, dummyHeadNode.Next, &LinkNode{}
+	for curr.Next != nil {
+		temp = curr.Next
+		curr.Next = temp.Next
+		temp.Next = pre.Next
+		pre.Next = temp
+	}
+
+	return dummyHeadNode.Next
+}
+
+func TestReverseListV5(t *testing.T) {
+	node := InitLinkNodeByList([]int{1, 2, 3, 4})
+	node.PrintList()
+
+	node = reverseListV5(node)
+	fmt.Println("**after**")
+
+	node.PrintList()
+}
+
+// 递归
+func reverseListV5(node *LinkNode) *LinkNode {
+	if node.Next == nil || node == nil {
+		return node
+	}
+
+	tail := reverseListV5(node.Next)
+
+	node.Next.Next = node
+	node.Next = nil
+
+	return tail
+}
